ccaab876e7727f8df9f2cb33022290a2ac838c9a
nurali-code <nur3.dav.97@gmail.com>
2025-05-16 17:26:52 +0500

    Добавлены новые секции "Ваши близкие друзья" и "Компании, работающие с нами" в HTML, включая слайдеры и маркеры. Обновлены стили для адаптивности и добавлены новые изображения. Интегрирован jQuery и Slick для функциональности слайдера.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 // Меню бургер
-document.querySelector('.btn__menu').addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.querySelector('.header .nav').classList.toggle('active');
+$('.btn__menu').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header .nav, .header').toggleClass('active');
+});
+
+$('.fr__slider').slick({
+    slidesToShow: 5,
+    infinite: true,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+    centerMode: true,
+    centerPadding: '6px',
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
 });
