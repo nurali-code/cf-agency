e5ed50b9d1a2fd726f3fbfd240d58abb4878cf69
nurali-code <nur3.dav.97@gmail.com>
2025-05-20 17:00:46 +0500

    Add new friend slider section and update styles
    - Added five new images for friends in the img directory.
    - Introduced a new section in index.html for displaying friends with a slider.
    - Implemented a second slider using Slick for the new friends section in script.js.
    - Updated styles in _elements.sass and _slick.sass for improved layout and responsiveness.
    - Enhanced the overall design in style.sass, including adjustments for mobile views.
    - Commented out unused keyframes in _marquee.sass to clean up the code.

--- js/script.js
+++ js/script.js
@@ -33,3 +33,49 @@ $('.fr__slider').slick({
         }
     ]
 });
+
+$('.fr__slider--second').slick({
+    slidesToShow: 5,
+    infinite: true,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+    centerMode: true,
+    centerPadding: '6px',
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 3,
+                variableWidth: true,
+                infinite: false,
+                centerMode: false,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                variableWidth: true,
+                infinite: false,
+                centerMode: false,
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 720,
+            settings: {
+                variableWidth: true,
+                infinite: false,
+                dots: true,
+                centerMode: false,
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
