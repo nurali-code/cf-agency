509c7f5c0284358d5c3de6b490eaade178f55c7e
nurali-code <nur3.dav.97@gmail.com>
2025-05-27 12:27:10 +0500

    Update team member profiles and slider settings
    - Changed names and positions of team members in the HTML, updating descriptions to reflect their roles and contributions.
    - Adjusted the slider settings in the JavaScript to show 4 slides instead of 5 for better visibility.
    - Added new styles for buttons and adjusted margins in the SASS files for improved layout and responsiveness.
    - Enhanced font sizes and spacing for better readability on smaller screens.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $('.btn__menu, header .nav__list-link').on('click', function () {
 });
 
 $('.fr__slider').slick({
-    slidesToShow: 5,
+    slidesToShow: 4,
     infinite: true,
     slidesToScroll: 1,
     arrows: false,
@@ -12,12 +12,6 @@ $('.fr__slider').slick({
     centerMode: true,
     centerPadding: '6px',
     responsive: [
-        {
-            breakpoint: 1600,
-            settings: {
-                slidesToShow: 4,
-            }
-        },
         {
             breakpoint: 1300,
             settings: {
